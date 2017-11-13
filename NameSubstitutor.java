4a49557c9d874a5405eaf1dea613577729e5daaa
AlexOmelchuk <sasha.miroshnichenko2194@gmail.com>
2017-11-13 13:03:30 +0200

    Initial commit

---
+++ NameSubstitutor.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NameSubstitutor {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enetr message: ");
+        String message = s.nextLine();
+        System.out.print("Enter name: ");
+        String name = s.nextLine();
+        String rename = message.replace("${name}", name);
+        s.close();
+
+        System.out.println(rename);
+
+    }
+}
